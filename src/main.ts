650e8aa14d70a8da58b7c2a577bc36322647690f
what1s1ove <zubko.2007@gmail.com>
2020-08-08 13:15:35 +0300

    module3-task1: + output more task functionality

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,8 @@ import { createTaskTemplate } from '~/view/task/task';
 import { createLoadMoreButtonTemplate } from '~/view/load-more-button/load-more-button';
 import { AdjacentHTMLPlace } from './common/enums';
 
-const TASK_COUNT = 4;
+const TASK_COUNT = 22;
+const TASK_COUNT_PER_STEP = 8;
 
 const tasks = generateTasks(TASK_COUNT);
 const filters = generateFilters(tasks);
@@ -40,7 +41,7 @@ renderTemplate(
   AdjacentHTMLPlace.BEFORE_END
 );
 
-for (let i = 1; i < TASK_COUNT; i++) {
+for (let i = 1; i < Math.min(tasks.length, TASK_COUNT_PER_STEP); i++) {
   renderTemplate(
     taskListElement,
     createTaskTemplate(tasks[i]),
@@ -48,8 +49,34 @@ for (let i = 1; i < TASK_COUNT; i++) {
   );
 }
 
-renderTemplate(
-  boardElement,
-  createLoadMoreButtonTemplate(),
-  AdjacentHTMLPlace.BEFORE_END
-);
+if (tasks.length > TASK_COUNT_PER_STEP) {
+  let renderedTaskCount = TASK_COUNT_PER_STEP;
+
+  renderTemplate(
+    boardElement,
+    createLoadMoreButtonTemplate(),
+    AdjacentHTMLPlace.BEFORE_END
+  );
+
+  const loadMoreBtn = boardElement.querySelector(`.load-more`);
+
+  const onShowMoreBtnClick = () => {
+    tasks
+      .slice(renderedTaskCount, renderedTaskCount + TASK_COUNT_PER_STEP)
+      .map((it) => renderTemplate(
+        taskListElement,
+        createTaskTemplate(it),
+        AdjacentHTMLPlace.BEFORE_END
+      ))
+      .join(``);
+
+    renderedTaskCount += TASK_COUNT_PER_STEP;
+
+    if (renderedTaskCount >= tasks.length) {
+      loadMoreBtn.removeEventListener(`click`, onShowMoreBtnClick);
+      loadMoreBtn.remove();
+    }
+  };
+
+  loadMoreBtn.addEventListener(`click`, onShowMoreBtnClick);
+}
