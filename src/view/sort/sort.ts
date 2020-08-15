a360eb4a18f8d7bf1773c4d4a8c5121d4932959c
what1s1ove <zubko.2007@gmail.com>
2020-08-15 19:49:08 +0300

    modyle5-task1: * sort class

--- src/view/sort/sort.ts
+++ src/view/sort/sort.ts
@@ -1,37 +1,25 @@
-import { createElement } from '~/helpers';
+import AbstractView from '~/view/abstract/abstract';
 import { SortType } from '~/common/enums';
 
-class Sort {
-  #element: Element | null;
-
+class Sort extends AbstractView {
   #sorts: SortType[];
 
   constructor(sorts: SortType[]) {
+    super();
     this.#sorts = sorts;
-    this.#element = null;
-  }
-
-  get node() {
-    if (!this.#element) {
-      this.#element = createElement(this.template);
-    }
-
-    return this.#element;
   }
 
   get template() {
+    const sortTemplates = this.#sorts.reduce((acc, it) => (acc.concat(`
+      <a href="#" class="board__filter">SORT BY ${it}</a>
+    `)), ``);
+
     return `
       <div class="board__filter-list">
-        ${this.#sorts.reduce((acc, it) => (acc.concat(`
-          <a href="#" class="board__filter">SORT BY ${it}</a>
-        `)), ``)}
+        ${sortTemplates}
       </div>
     `;
   }
-
-  public removeElement = () => {
-    this.#element = null;
-  };
 }
 
 export default Sort;
