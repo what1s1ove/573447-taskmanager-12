be64ed7f349f5f37211a5b331e2e021cc8c36757
what1s1ove <zubko.2007@gmail.com>
2020-08-15 19:44:21 +0300

    modyle5-task1: * no-tasks class

--- src/view/no-tasks/no-tasks.ts
+++ src/view/no-tasks/no-tasks.ts
@@ -1,30 +1,12 @@
-import { createElement } from '~/helpers';
+import AbstractView from '~/view/abstract/abstract';
 
-class NoTask {
-  #element: Element | null;
-
-  constructor() {
-    this.#element = null;
-  }
-
-  get node() {
-    if (!this.#element) {
-      this.#element = createElement(this.template);
-    }
-
-    return this.#element;
-  }
-
+class NoTask extends AbstractView {
   get template() {
     return `
       <p class="board__no-tasks">
         Click «ADD NEW TASK» in menu to create your first task
       </p>
     `;
-  }
-
-  public removeElement() {
-    this.#element = null;
   }
 }
 
