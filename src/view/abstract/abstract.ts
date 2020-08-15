df547952a1844ce2d0d28b2d4c0f6632cb2bad62
what1s1ove <zubko.2007@gmail.com>
2020-08-15 19:30:03 +0300

    modyle5-task1: + abstract methods

--- src/view/abstract/abstract.ts
+++ src/view/abstract/abstract.ts
@@ -1,8 +1,28 @@
+import { createElement } from '~/helpers';
+
 abstract class Abstract {
+  abstract get template(): string;
+
+  #element: Element | null;
+
   constructor() {
     if (new.target === Abstract) {
       throw new Error(`Can't instantiate Abstract, only concrete one.`);
     }
+
+    this.#element = null;
+  }
+
+  get node() {
+    if (!this.#element) {
+      this.#element = createElement(this.template);
+    }
+
+    return this.#element;
+  }
+
+  public removeElement() {
+    this.#element = null;
   }
 }
 
