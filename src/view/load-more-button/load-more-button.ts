36f218a29f9a9fee511a6b3751e9638cb374eac0
what1s1ove <zubko.2007@gmail.com>
2020-08-15 19:43:35 +0300

    modyle5-task1: * load more button class

--- src/view/load-more-button/load-more-button.ts
+++ src/view/load-more-button/load-more-button.ts
@@ -1,30 +1,12 @@
-import { createElement } from '~/helpers';
+import AbstractView from '~/view/abstract/abstract';
 
-class LoadMoreButton {
-  #element: Element | null;
-
-  constructor() {
-    this.#element = null;
-  }
-
-  get node() {
-    if (!this.#element) {
-      this.#element = createElement(this.template);
-    }
-
-    return this.#element;
-  }
-
+class LoadMoreButton extends AbstractView {
   get template() {
     return `
       <button class="load-more" type="button">
         load more
       </button>
     `;
-  }
-
-  public removeElement() {
-    this.#element = null;
   }
 }
 
